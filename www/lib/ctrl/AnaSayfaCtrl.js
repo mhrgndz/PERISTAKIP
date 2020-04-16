fcdef5b6305f352bfd0d19676e787640dc3aafc6
Mahir Gündüz <41637496+mhrgndz@users.noreply.github.com>
2020-04-17 01:53:38 +0300

    .

--- www/lib/ctrl/AnaSayfaCtrl.js
+++ www/lib/ctrl/AnaSayfaCtrl.js
@@ -2,7 +2,7 @@ function AnaSayfaCtrl($scope,$window,db)
 {
     function Init()
     {
-        
+        $scope.Tarih = moment(new Date()).format("DD.MM.YYYY");
     }
    
     $scope.Yeni = function()
--- www/lib/rapor/IscilikOzetRapor.js
+++ www/lib/rapor/IscilikOzetRapor.js
@@ -23,11 +23,11 @@ function IscilikOzetRapor($scope,$window,db)
     {
         var TmpQuery = 
         {
-            query : "SELECT COUNT(KODU) AS TOPLAM, " +
+            query : "SELECT ISNULL(COUNT(KODU),0) AS TOPLAM, " +
                     "TIP AS TIP, " +
                     "CASE WHEN TIP = 1 THEN 'SÖZLEŞMELİ' WHEN TIP = 2 THEN 'İLAVE' WHEN TIP = 3 THEN 'YEVMİYE' END AS TIPADI " +
                     "FROM ISEMRIHAREKET " +
-                    "WHERE GEMIKODU = @GEMIKODU AND TIP <> 2 GROUP BY TIP " ,
+                    "WHERE GEMIKODU = @GEMIKODU AND TIP <> 2 GROUP BY TIP ORDER BY TIP" ,
             param : ['GEMIKODU'],
             type : ['string|25'],
             value:  [$scope.GemiKodu]
@@ -36,9 +36,16 @@ function IscilikOzetRapor($scope,$window,db)
         db.GetDataQuery(TmpQuery,async function(Data)
         {   
             $scope.RaporList = Data
-            $scope.SozlesmeliToplam = $scope.RaporList[0].TOPLAM
-            $scope.YevmiyeToplam = $scope.RaporList[1].TOPLAM
-            $scope.Toplam = $scope.SozlesmeliToplam + $scope.YevmiyeToplam;
+            if($scope.RaporList.length > 0)
+            {
+                $scope.SozlesmeliToplam = $scope.RaporList[0].TOPLAM
+                $scope.YevmiyeToplam = $scope.RaporList[1].TOPLAM
+                $scope.Toplam = $scope.SozlesmeliToplam + $scope.YevmiyeToplam;
+            }
+            else
+            {
+                alertify.alert("Gösterilecek Veri Bulanamadı.");
+            }
         });
     }
     $scope.Yeni = async function()
@@ -47,16 +54,19 @@ function IscilikOzetRapor($scope,$window,db)
 
         await GemiGetir();
     }
-    $scope.BtnCalistir = function()
+    $scope.BtnCalistir = async function()
     {
+        $scope.SozlesmeliToplam = "0";
+        $scope.YevmiyeToplam = "0";
+        $scope.Toplam = "0";
+
         if($scope.GemiKodu != '0')
         {
-            RaporGetir();
+            await RaporGetir();
         }
         else
         {
             alertify.alert("Lütfen Gemi Seçimi Yapınız.");
         }
-        
     }
 }
